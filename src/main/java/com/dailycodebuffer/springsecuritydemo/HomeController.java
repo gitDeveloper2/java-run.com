18ff8cbfb527931e0f80dd94a90c90907d36c982
horace karatu <horace.karatu@gmail.com>
2022-12-12 18:32:41 +0300

    template complete

--- src/main/java/com/dailycodebuffer/springsecuritydemo/HomeController.java
+++ src/main/java/com/dailycodebuffer/springsecuritydemo/HomeController.java
@@ -16,56 +16,71 @@ import org.springframework.web.servlet.ModelAndView;
 @RestController
 
 public class HomeController {
-@Autowired
-private CustomUserDetailsService userDetailsService;
-@Autowired
-private PasswordEncoder passwordEncoder;
-	@GetMapping("/")
-	public ModelAndView index () {
-	    ModelAndView modelAndView = new ModelAndView();
-	    modelAndView.setViewName("index.html");
-	    return modelAndView;
-    }
+	@Autowired
+	private CustomUserDetailsService userDetailsService;
+	@Autowired
+	private PasswordEncoder passwordEncoder;
+
+	@GetMapping("/test")
+	public ModelAndView test() {
+		ModelAndView modelAndView = new ModelAndView();
+		modelAndView.setViewName("/fragments/header.html");
+		return modelAndView;
+	}
 	
-    @GetMapping("/home")
-    public String home(){
-        return "This is Home Page";
-    }
+	@GetMapping("/")
+	public ModelAndView index() {
+		ModelAndView modelAndView = new ModelAndView();
+		modelAndView.setViewName("index.html");
+		return modelAndView;
+	}
 
-    @GetMapping("/admin")
-    public String admin(){
-        return "This is Admin Page";
-    }
+	@GetMapping("/content")
+	public ModelAndView content() {
+		ModelAndView modelAndView = new ModelAndView();
+		modelAndView.setViewName("/website/index.html");
+		return modelAndView;
+	}
 
-    @GetMapping("/login")
-    public ModelAndView login(){
-	    ModelAndView modelAndView = new ModelAndView();
-	    modelAndView.setViewName("login.html");
-	    return modelAndView;
-    }
-    
+	@GetMapping("/home")
+	public String home() {
+		return "This is Home Page";
+	}
 
-    @GetMapping("/register")
-    public ModelAndView register(){
-	    ModelAndView modelAndView = new ModelAndView();
-	    modelAndView.setViewName("login.html");
-	    return modelAndView;
-    }
-   @RequestMapping(value = "/adduser", method = {RequestMethod.POST})
-    public ModelAndView addUser(@RequestParam Map<String, String> auth){
-	   String username=auth.get("username");
-	   String password=auth.get("password");
-	   
-	   User user=new User();
-	   user.setUsername(username);
-	   user.setPassword(passwordEncoder.encode(password));
-	   user.setRole("USER");
-    	userDetailsService.createUser(user);
-    	
-    	 ModelAndView modelAndView = new ModelAndView();
- 	    modelAndView.setViewName("login.html");
- 	    return modelAndView;
-    	
-    }
-    
+	@GetMapping("/admin")
+	public String admin() {
+		return "This is Admin Page";
+	}
+
+	@GetMapping("/login")
+	public ModelAndView login() {
+		ModelAndView modelAndView = new ModelAndView();
+		modelAndView.setViewName("login.html");
+		return modelAndView;
+	}
+
+	@GetMapping("/register")
+	public ModelAndView register() {
+		ModelAndView modelAndView = new ModelAndView();
+		modelAndView.setViewName("login.html");
+		return modelAndView;
+	}
+
+	@RequestMapping(value = "/adduser", method = { RequestMethod.POST })
+	public ModelAndView addUser(@RequestParam Map<String, String> auth) {
+		String username = auth.get("username");
+		String password = auth.get("password");
+
+		User user = new User();
+		user.setUsername(username);
+		user.setPassword(passwordEncoder.encode(password));
+		user.setRole("USER");
+		userDetailsService.createUser(user);
+
+		ModelAndView modelAndView = new ModelAndView();
+		modelAndView.setViewName("login.html");
+		return modelAndView;
+
+	}
+
 }
